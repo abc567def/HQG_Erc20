05eaf774ba1f56c7f9864e4e8d0b201a59d81b75
people <abc567def>
2020-07-19 18:30:39 +0800

    first commit

---
+++ curl/tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = 'gcc -E -I/home/xialamp/桌面/rexx2.0/rexx/src/3rd/openssl/include -I/home/xialamp/桌面/rexx2.0/rexx/src/3rd/openssl/include/openssl';
+
+1;
